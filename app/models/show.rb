89e8f4c84255a35e48385cc0ffda04f55a131837
James Scalise <jamescscalise@gmail.com>
2019-11-12 20:08:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,7 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  belongs_to :network
   
+  def actors_list
+  end
 end
---
+++ db/migrate/007_add_day_season_and_genre_to_shows.rb
@@ -0,0 +1,7 @@
+class AddDaySeasonAndGenreToShows < ActiveRecord::Migration[5.1]
+  class change
+    add_column :shows, :catchphrase, :string
+    add_column :shows, :catchphrase, :string
+    add_column :shows, :catchphrase, :string
+  end
+end
