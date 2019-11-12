1abb70da3b974a12e7bda35bdb74704001e244be
James Scalise <jamescscalise@gmail.com>
2019-11-12 20:35:53 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_season_and_genre_to_shows.rb
+++ db/migrate/007_add_day_season_and_genre_to_shows.rb
@@ -1,7 +1,7 @@
 class AddDaySeasonAndGenreToShows < ActiveRecord::Migration[5.1]
   class change
     add_column :shows, :genre, :string
-    add_column :shows, :catchphrase, :string
-    add_column :shows, :catchphrase, :string
+    add_column :shows, :season, :string
+    add_column :shows, :day, :string
   end
 end
